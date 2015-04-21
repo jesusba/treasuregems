455765b3be332eb52b4b0a0f04b2230b8b8c34cb
Jesús Bueno <jesusba60@gmail.com>
2015-04-21 10:54:32 +0200

    Añadida nueva ruta

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -33,6 +33,23 @@ def info():
 	return template('info.tpl',nombre=nombre,version=version,descargas=descargas,
 		autor=autor,descripcion=descripcion,dependencias=dependencias)
 
+@get('/history')
+	gemaversion=request.params.get('gem')
+	url_historial="http://rubygems.org/api/v1/versions/"
+
+	rhistorial=requests.get(url_info+gemaversion+".json")
+	dochistorial = json.loads(rinfo.text)
+
+	hnombre=dochistorial["name"]
+	hversion=dochistorial["number"]
+	hdescargas=dochistorial["version_downloads"]
+	hautor=dochistorial["authors"]
+	hfechapubli=dochistorial["built_at"]
+	confechapubli = hfechapubli[8:10]+"-"+hfechapubli[5:8]+hfechapubli[0:4]+" "+hfechapubli[11:16]
+
+	return template('history.tpl',hnombre=hnombre,hversion=hversion,hdescargas=hdescargas,
+		hautor=hautor,confechapubli=confechapubli)
+
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
